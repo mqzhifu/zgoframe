eeba82be5b14c6cb6f803390323af84744c4539c
mqzhifu <mqzhifu@sina.com>
2022-01-10 16:51:55 +0800

    ddd

---
+++ cp_project.sh
@@ -0,0 +1,32 @@
+#./cp_project.sh /data/www/golang/src/zgoframe /data/www/golang/src/logslave
+
+ORI_PROJECT_DIR=$1
+TAR_GET_DIR=$2
+
+echo "ORI_PROJECT_DIR:$ORI_PROJECT_DIR TAR_GET_DIR:$TAR_GET_DIR"
+
+#先把目标目录下的目录清空下
+cd $TAR_GET_DIR
+
+ls -l
+
+rm -rf *
+rm -rf .drone.yml .gitignore
+
+
+
+cd $ORI_PROJECT_DIR
+
+targetZipFileFullName="${TAR_GET_DIR}/git_project.zip"
+
+echo "targetZipFileFullName:$targetZipFileFullName"
+
+`git archive --format zip  --output "$targetZipFileFullName" master`
+
+cd $TAR_GET_DIR
+ls -l
+
+unzip $targetZipFileFullName
+rm -rf $targetZipFileFullName
+
+cat ${$ORI_PROJECT_DIR}/config.toml |sed 's/projectId = 6/projectId = 3/'

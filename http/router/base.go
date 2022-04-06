40aba8c7928d28c04f6189d44b5bcd9ae3193574
mqzhifu <mqzhifu@sina.com>
2022-04-06 18:17:30 +0800

    ddd

--- http/router/base.go
+++ http/router/base.go
@@ -37,23 +37,3 @@ func InitBaseRouter(Router *gin.RouterGroup) {
 
 	}
 }
-
-func InitToolsRouter(Router *gin.RouterGroup) {
-	ToolsRouter := Router.Group("tools")
-	{
-		//牛课网
-		ToolsRouter.GET("niuke/question/dir/list", v1.NiukeQuestionDirList)
-		//header头结构体 - 用于测试
-		ToolsRouter.GET("header/struct", v1.HeaderStruct)
-		//所有常量列表
-		ToolsRouter.GET("const/list", v1.ConstList)
-		//所有常量列表 - 生成MYSQL 脚本
-		ToolsRouter.GET("const/init/db", v1.ConstInitDb)
-		//获取APP 列表
-		ToolsRouter.POST("project/list", v1.ProjectList)
-		//获取APP 列表
-		ToolsRouter.POST("project/info/{id}", v1.ProjectOneInfo)
-		//获取APP 列表
-		ToolsRouter.POST("frame/sync/js/demo", v1.FrameSyncJsDemo)
-	}
-}

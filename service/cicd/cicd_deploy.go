fa2f5e41cc232b76bb871f30debc6495e3a04b7e
mqzhifu <mqzhifu@sina.com>
2022-07-20 19:29:04 +0800

    ddd

--- service/cicd/cicd_deploy.go
+++ service/cicd/cicd_deploy.go
@@ -177,8 +177,8 @@ func (deploy *Deploy) CheckTest(server util.Server, serviceDeployConfig ServiceD
 		util.MyPrint(errMsg)
 		return errors.New(errMsg)
 	}
-
-	test_allow_project_name := []string{"Zgoframe"}
+	//目前仅允许这3个项目部署，3个全开放，是给HTTP使用，指令行测试，把下面两个打开即可
+	test_allow_project_name := []string{"Zgoframe", "Zwebuivue", "Zwebuivgo"}
 	//test_allow_project_name := []string{"Zwebuivue"}
 	//test_allow_project_name := []string{"Zwebuivgo"}
 	for _, v := range test_allow_project_name {

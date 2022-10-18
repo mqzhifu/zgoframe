7545e8fd5293c649d0ac4471a285cf4ff25c6c4c
mqzhifu <mqzhifu@sina.com>
2022-10-18 12:48:25 +0800

    ddd

---
+++ http/router/gateway.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	v1 "zgoframe/api/v1"
+)
+
+func InitGatewayRouter(Router *gin.RouterGroup) {
+
+	GatewayRouter := Router.Group("gateway")
+	{
+		GatewayRouter.POST("service/:service_name/:func_name", v1.GatewayService)
+		GatewayRouter.GET("proto", v1.GatewayProto)
+		GatewayRouter.GET("action/map", v1.ActionMap)
+		GatewayRouter.GET("config", v1.GatewayConfig)
+		GatewayRouter.GET("fd/list", v1.GatewayFDList)
+		GatewayRouter.POST("send/msg", v1.GatewaySendMsg)
+
+	}
+}

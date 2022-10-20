2ec8a2e2a833597ec4f33463b706c547d2f614a1
mqzhifu <mqzhifu@sina.com>
2022-10-20 15:25:19 +0800

    ddd

--- model/cicd_publish.go
+++ model/cicd_publish.go
@@ -7,8 +7,8 @@ type CicdPublish struct {
 	ServerId     int    `json:"server_id" form:"server_id" db:"define:int;comment:服务器ID;defaultValue:0"`
 	Status       int    `json:"status" form:"status" db:"define:tinyint(1);comment:1待部署2待发布3已发布4发布失败;defaultValue:0"`
 	DeployStatus int    `json:"deploy_status" form:"deploy_status" db:"define:tinyint(1);comment:1部署中2失败3完成;defaultValue:0"`
-	ServiceInfo  string `json:"service_info" form:"service_info" db:"define:varchar(255);comment:服务信息-备份;defaultValue:''"`
-	ServerInfo   string `json:"server_info" form:"server_info" db:"define:varchar(255);comment:服务器信息-备份;defaultValue:''"`
+	ServiceInfo  string `json:"service_info" form:"service_info" db:"define:text;comment:服务信息-备份"`
+	ServerInfo   string `json:"server_info" form:"server_info" db:"define:text;comment:服务器信息-备份"`
 	DeployType   int    `json:"deploy_type" form:"status" db:"define:tinyint(1);comment:1本地部署2远程同步部署;defaultValue:0"`
 	CodeDir      string `json:"code_dir" form:"code_dir" db:"define:varchar(255);comment:项目代码目录名;defaultValue:''"`
 	Step         int    `json:"step" form:"step" db:"define:int;comment:执行到第几步了;defaultValue:0"`
